53f80b3796022e84b2313a0d7578b8374bf56223
jotas <jotasenator@gmail.com>
2021-08-23 00:50:29 -0400

    Show data in table and configure css and bootstrap attributes

--- src/components/Table.js
+++ src/components/Table.js
@@ -12,10 +12,11 @@ export default function Tabla() {
     const { data } = useSelector(state => state.axiosDataReducer)
 
     return (
-        <div>
-            <table className="table table-striped">
-                <thead>
-                    <tr >
+        <div className='container mt-5 mb-5'>
+            <table className="table table-striped table-hover caption-top ">
+                <caption>Online Store</caption>
+                <thead className='table-dark'>
+                    <tr className='text-center'>
                         <th scope="col">Id</th>
                         <th scope="col">Name</th>
                         <th scope="col">Cost</th>
@@ -29,15 +30,15 @@ export default function Tabla() {
                             <tr key={x.id}>
                                 <th scope="col">{x.id}</th>
                                 <th scope="col">{x.name}</th>
-                                <th scope="col">{x.cost}</th>
-                                <th>
+                                <th scope="col">$ {x.cost.toFixed(2)}</th>
+                                <th className='text-center'>
                                     {
                                         x.category.map((y, index) => (
                                             <span key={index * 0.125}>{y.name}</span>
                                         ))
                                     }
                                 </th>
-                                <th>
+                                <th className='text-center'>
                                     {
                                         x.department.map((z, index) => (
                                             <span key={index * 0.225}>{z.name}</span>
