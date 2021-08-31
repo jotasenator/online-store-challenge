06eb6e45933a2ff66e32db38755557f3022c169b
jotas <jotasenator@gmail.com>
2021-08-31 11:38:50 -0400

    modify Skeleton count

--- src/components/skeletons/LoadingSkeletonUpdate.js
+++ src/components/skeletons/LoadingSkeletonUpdate.js
@@ -4,7 +4,7 @@ import Skeleton from 'react-loading-skeleton';
 export default function LoadingSkeletonUpdate() {
     return (
         <div className='container mt-5 mb-5 pt-3 pb-3 '>
-            <Skeleton count={7} height={60} />
+            <Skeleton count={6} height={60} />
             <div className='head-table-skeleton'>
                 <div><Skeleton width={210} height={40} /></div>
             </div>
