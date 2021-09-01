b8854a3d4d6dd3e0a4fb1409de585a201c3e1a44
jotas <jotasenator@gmail.com>
2021-08-31 21:21:26 -0400

    Refactor LoadingSkeletonUpdate

--- src/tests/components/skeletons/LoadingSkeletonUpdate.test.js
+++ src/tests/components/skeletons/LoadingSkeletonUpdate.test.js
@@ -6,18 +6,24 @@ import LoadingSkeletonUpdate from '../../../components/skeletons/LoadingSkeleton
 
 
 describe('Snapshot from LoadingSkeletonUpdate', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<LoadingSkeletonUpdate />);
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+    });
 
     test('should render <LoadingSkeletonUpdate/> correctly', () => {
-        const wrapper = shallow(<LoadingSkeletonUpdate />)
         expect(wrapper).toMatchSnapshot()
     })
 
     test('should show me what is inside className=container', () => {
-        const wrapper = shallow(<LoadingSkeletonUpdate />)
         expect(wrapper.find('.container').text()).toBe('<Skeleton /><Skeleton />')
     })
     test('should show me what is inside className=head-table-skeleton', () => {
-        const wrapper = shallow(<LoadingSkeletonUpdate />)
         expect(wrapper.find('.head-table-skeleton').text()).toBe('<Skeleton />')
     })
 
